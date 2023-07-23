3dd193825fe4a5408b9cf2363dcf1b6068709fa3
22bca221 <127925794+22bca221@users.noreply.github.com>
2023-07-23 17:16:14 +0530

    Update script4.py

--- SOLUTION1/script4.py
+++ SOLUTION1/script4.py
@@ -1,12 +1,13 @@
+# Write a script to enter any number and check its armstrong number or not.
 a=int(input("enter any number"))
-sum=0
+s=0
 temp=a
 while(a>0):
      b=a%10
      c=b*b*b
      a=a/10
-     sum=sum=c
-if(sum==temp):
+     s=s=c
+if(s==temp):
       print("{} is armstrong".format(temp))
 else:
       print("number is not armstrong:")
