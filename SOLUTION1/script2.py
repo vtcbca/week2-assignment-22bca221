22e48c43fa40b7994a4c7fad24f225f1c783e662
22bca221 <127925794+22bca221@users.noreply.github.com>
2023-07-23 17:09:42 +0530

    Update script2.py

--- SOLUTION1/script2.py
+++ SOLUTION1/script2.py
@@ -1,7 +1,8 @@
-a=int(input("Enter any number:"))
-sum=0
-while(a>0):
-    b=a%10
-    sum=sum+b
-    a=a/10
+#Write a python script to enter any number and print the sum of its dig.
+r=int(input("Enter any number:"))
+s=0
+while(r>0):
+    c=r%10
+    s=s+c
+    r=r/10
 print("sum of digit of no {}is{}".format(a,sum))   
