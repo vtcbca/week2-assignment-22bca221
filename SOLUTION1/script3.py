8658d06795a271c07c3369c52621d126a9846fb0
22bca221 <127925794+22bca221@users.noreply.github.com>
2023-07-23 17:13:40 +0530

    Update script3.py

--- SOLUTION1/script3.py
+++ SOLUTION1/script3.py
@@ -1,12 +1,13 @@
-a=int(input("Enter any numbre:"))
-c=a 
+# Write a python script to enter any number,if it is interger number,then check its palindrome or not.Print appropriate message if it is not palindrome.
+s=int(input("Enter any number:"))
+a=s
 r=0
 while(r>0):
     digit=n%10
     r=r*10+digit
-    a=a//10
-print(a)
-if r==c:
+    s=s//10
+print(s)
+if r==a:
     print("number is palindrome")
 else:
     print("number is not palindrome")
